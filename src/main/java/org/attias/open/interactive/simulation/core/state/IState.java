6c52380484c2cd0efacee4cb2cd088ae36b214bc
Assaf Attias <assafattias93@gmail.com>
2023-08-19 09:57:07 +0300

    Add States

--- src/main/java/org/attias/open/interactive/simulation/core/state/IState.java
+++ src/main/java/org/attias/open/interactive/simulation/core/state/IState.java
@@ -1,5 +1,16 @@
 package org.attias.open.interactive.simulation.core.state;
 
 public interface IState {
+
+    boolean enter(Object... parameters);
+    void exit();
+
+    void pause();
+    void resume();
+
+    void resize(int width, int height);
     void render();
+    boolean update(float dt);
+
+    void dispose();
 }
